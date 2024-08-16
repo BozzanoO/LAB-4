5ac902fe62298379c9f64fa673153df92f67256b
PedroBozzano <pedro.bozzano08@gmail.com>
2024-08-15 21:53:03 -0300

    first commit

---
+++ src/main/java/br/fatec/labiv/Project_LAB4/ProjectLab4Application.java
@@ -0,0 +1,13 @@
+package br.fatec.labiv.Project_LAB4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/labiv/Project_LAB4/ProjectLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.labiv.Project_LAB4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
